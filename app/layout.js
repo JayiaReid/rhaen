86ec0499ffa976152b96a816fa6053f2e6af75c0
JayiaReid <jayiareid24@gmail.com>
2024-09-14 23:29:12 -0400

    .

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
 import Nav from "./_components/Nav";
 import { ThemeProvider } from "./Theme";
